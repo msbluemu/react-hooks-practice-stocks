0a5b76f9adaf4e37f7eeed0df55bd49833eed120
Qianyun Mu <msbluemu@gmail.com>
2023-07-18 17:20:58 +1000

    complete lab

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,6 +6,9 @@ import SearchBar from "./SearchBar";
 function MainContainer() {
   const [stocks, setStocks] = useState([]);
   const [portfolioStocks, setPortfolioStocks] = useState([]);
+  const [sortedStocks, setSortedStocks] = useState([...stocks]);
+  const [filteredStocks, setFilteredStocks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
   
   useEffect(() => {
     fetch(" http://localhost:3001/stocks")
@@ -13,6 +16,22 @@ function MainContainer() {
      .then((data) => setStocks(data));
   }, []);
  
+  
+    const handleSort = (sortType) => {
+      // Perform sorting logic based on the selected sort type
+      let sortedData = [];
+  
+      if (sortType === "Alphabetically") {
+        sortedData = stocks.sort((a, b) =>
+          a.ticker.localeCompare(b.ticker)
+        );
+      } else if (sortType === "Price") {
+        sortedData = stocks.sort((a, b) => a.price - b.price);
+      }
+      setSortedStocks([...sortedData]);
+    };
+  
+
   function handleClickStock(stock) {
     setPortfolioStocks([...portfolioStocks, stock]);
   }
@@ -23,14 +42,27 @@ function MainContainer() {
     );
     setPortfolioStocks(updatedStocks);
   }
+  
+  const handleCategoryChange = (e) => {
+    const category = e.target.value;
+
+    setSelectedCategory(category);
+
+    if (category === "All") {
+      setFilteredStocks(stocks);
+    } else {
+      const filtered = stocks.filter((stock) => stock.type === category);
+      setFilteredStocks(filtered);
+    }
+  };
 
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSort={handleSort} selectedCategory={selectedCategory} handleCategory={handleCategoryChange}/>
       <div className="row">
         <div className="col-8">
           <StockContainer 
-            stocks={stocks} 
+            stocks={selectedCategory === "All" ? stocks : filteredStocks} 
             onClickStock={handleClickStock}
             />
         </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
-function SearchBar() {
+function SearchBar({onSort, selectedCategory, handleCategory}) {
+  const [selectedSort, setSelectedSort] = useState("");
+
+  const handleSortChange = (e) => {
+    const sortType = e.target.value;
+    setSelectedSort(sortType);
+    onSort(sortType);
+  };
+
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +18,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={selectedSort === "Alphabetically"}
+          onChange={handleSortChange}
         />
         Alphabetically
       </label>
@@ -19,15 +28,16 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={selectedSort === "Price"}
+          onChange={handleSortChange}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleCategory} value={selectedCategory}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
